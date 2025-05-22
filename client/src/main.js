d85d9824e2279a45adb8327d6fdf8edf7d98df20
az-m <alisonzmills@gmail.com>
2025-05-22 15:36:12 +0100

    feat: added function to show messages from db

--- client/src/main.js
+++ client/src/main.js
@@ -26,6 +26,12 @@ function openTab(pageName) {
 
   // make the one we selected visible again
   document.getElementById(pageName).style.display = "block";
+
+  if (pageName === "messagePage") {
+    showMessages();
+  } else {
+    removeMessages();
+  }
 }
 
 // ====================================================================================
@@ -54,3 +60,58 @@ function handleSubmit(event) {
 }
 
 form.addEventListener("submit", handleSubmit);
+
+// ====================================================================================
+
+// This section get the messages from the API and shows them
+
+async function showMessages() {
+  const messages = await getMessages();
+  makeMessageElements(messages);
+}
+
+async function getMessages() {
+  // go go get messages
+  const API = apiRoot + "messages";
+  const response = await fetch(API);
+  const messageData = await response.json();
+  return messageData;
+}
+
+function makeMessageElements(messageArr) {
+  // I'd like the newest messages at the top please
+  messageArr.reverse();
+
+  // creates the div for each upgrade item, populates it with the data, and adds it to the messages section in the dom
+  messageArr.forEach((item, index) => {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "message");
+    const section = document.getElementById("messages");
+    section.appendChild(newDiv);
+
+    const pName = document.createElement("p");
+    const pLocation = document.createElement("p");
+    const pMessage = document.createElement("p");
+    const pTimestamp = document.createElement("p");
+
+    pName.textContent = item.name;
+    pLocation.textContent = item.location;
+    pMessage.textContent = item.content;
+    pTimestamp.textContent = item.created_at;
+
+    // get the div we just added back from the DOM to add its content elements
+    const div = document.getElementsByClassName("message")[index];
+
+    div.appendChild(pName);
+    div.appendChild(pLocation);
+    div.appendChild(pMessage);
+    div.appendChild(pTimestamp);
+  });
+}
+
+// ====================================================================================
+
+function removeMessages() {
+  const section = document.getElementById("messages");
+  section.replaceChildren();
+}
